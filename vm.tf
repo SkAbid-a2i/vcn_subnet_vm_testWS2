c54f4d88da68372063906b781035b74448e53344
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-06 01:06:49 +0600

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
 resource "oci_core_instance" "my_vm" {
-  compartment_id      = var.compartment_ocid  # This will now reference the declared variable
-  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Example AD
-  shape               = "VM.Standard2.1"  # VM shape
-  display_name        = "MyVMab"
-  subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id
-  image_id            = ocid1.image.oc15.ap-dcc-gazipur-1.aaaaaaaaknrel32tnjjo4n3fuodfmxbehhwp336yafixsvm7etw2olsgterq # Image ID for the instance
+  compartment_id      = var.compartment_ocid  # Reference the compartment OCID from variables
+  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Specify Availability Domain (AD)
+  shape               = "VM.Standard2.1"  # Define the VM shape
+  display_name        = "MyVMab"  # Display name for your VM
+  subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id  # Reference subnet_id from Workspace 1
+  image_id            = var.image_id  # Use the image_id variable (this will reference the value in terraform.tfvars)
 }
