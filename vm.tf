596d04de89d60366f689e4af8ce316dd7f05e9ca
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-06 00:58:22 +0600

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
 resource "oci_core_instance" "my_vm" {
-  compartment_id      = var.compartment_ocid
-  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Use the correct AD based on your region
-  shape               = "VM.Standard2.1"  # Use the appropriate shape for your VM
-  display_name        = "MyVM"
-  subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id  # Public subnet ID from Workspace 1
-  image_id            = var.image_id  # Provide the correct image ID for the VM
+  compartment_id      = var.compartment_ocid  # This will now reference the declared variable
+  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Example AD
+  shape               = "VM.Standard2.1"  # VM shape
+  display_name        = "MyVMab"
+  subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id
+  image_id            = var.image_id  # Image ID for the instance
 }
