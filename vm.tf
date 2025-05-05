90a02e20c5dded91c707a8bcf9c0c60925f429e8
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-06 01:17:13 +0600

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,13 +1,13 @@
 resource "oci_core_instance" "my_vm" {
   compartment_id      = var.compartment_ocid  # The compartment where your instance will be created
-  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Update with the correct Availability Domain (AD)
-  shape               = "VM.Standard2.1"  # Example VM shape (you can change it based on your needs)
+  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Availability Domain (AD)
+  shape               = "VM.Standard2.1"  # VM shape (you can change it based on your needs)
   display_name        = "MyVMab"  # Name of your instance
   subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id  # Using the subnet ID from Workspace 1
 
-  # Use the image ID you found in the Oracle Cloud UI
+  # Source details block
   source_details {
-    source_type = "image"  # Specifying that the source for the instance is an image
+    source_type = "image"  # Image source type
     image_id    = "ocid1.image.oc15.ap-dcc-gazipur-1.aaaaaaaaknrel32tnjjo4n3fuodfmxbehhwp336yafixsvm7etw2olsgterq"  # Your image ID
   }
 
