54783d61c004de5816d334a209e7e4564cd2947b
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-06 00:16:58 +0600

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,14 +1,8 @@
 resource "oci_core_instance" "my_vm" {
-  compartment_id   = var.compartment_ocid
-  availability_domain = "Uocm:PHX-AD-1"  # Specify your availability domain
-  shape             = "VM.Standard2.1"
-  display_name      = "VM-${terraform.workspace}"
-  image_id          = var.image_ocid
-
-  # Use the subnet ID from Workspace 1
-  subnet_id         = data.terraform_remote_state.vcn_workspace_state.outputs.subnet_public_id
-}
-
-output "vm_id" {
-  value = oci_core_instance.my_vm.id
+  compartment_id      = var.compartment_ocid
+  availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Use the correct AD based on your region
+  shape               = "VM.Standard2.1"  # Use the appropriate shape for your VM
+  display_name        = "MyVM"
+  subnet_id           = data.terraform_remote_state.workspace_1.outputs.subnet_public_id  # Public subnet ID from Workspace 1
+  image_id            = var.image_id  # Provide the correct image ID for the VM
 }
